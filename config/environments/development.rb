71e5576a13426a6a63439989ba1ea08b39a36211
lavanya94lava <singhlavanya94@gmail.com>
2020-06-22 12:18:31 +0530

    remove personal credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
     {
         address:              'smtp.gmail.com',
         port:                 587,
-        user_name:            "singhlavanya94@gmail.com",
-        password:             "sudharana007!",
+        user_name:            ENV[GMAIL_USERNAME],
+        password:             ENV[GMAIL_PASSWORD],
         authentication:       'plain',
         enable_starttls_auto: true  
     }
